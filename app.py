6154f07402f7004b42370bf52a90819e8e0bc4c3
luis <ferrerluis97@gmail.com>
2016-03-27 02:40:27 -0400

    Made the app.py more reliable

--- app.py
+++ app.py
@@ -9,10 +9,14 @@ def index():
 
 @app.route('/item/<code>')
 def item(code):
-	item = Item.get(Item.code == code)
-	reviews = Review.select().where(Review.code == code)
+	try:
+		item = Item.get(Item.code == code)
 
-	return render_template('item.html', name=item.name, reviews=reviews, image_url=item.image_url, price=item.price)
+		reviews = Review.select().where(Review.code == code)
+
+		return render_template('item.html', name=item.name, reviews=reviews, image_url=item.image_url, price=item.price)
+	except Exception:
+		return "Does not exist"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port='5000')
